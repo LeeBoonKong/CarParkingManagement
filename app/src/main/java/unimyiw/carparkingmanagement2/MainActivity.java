0d22bd79da65c67789b26f052bd201822c30c7a1
LeeBoonKong <lee.boon.kong@hotmail.com>
2017-11-06 15:58:33 +0800

    all the changes

--- app/src/main/java/unimyiw/carparkingmanagement2/MainActivity.java
+++ app/src/main/java/unimyiw/carparkingmanagement2/MainActivity.java
@@ -3,6 +3,7 @@ package unimyiw.carparkingmanagement2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
