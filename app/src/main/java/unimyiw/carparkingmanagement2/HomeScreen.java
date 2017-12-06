255ad84841b17901cf20346f6d56a8fea1e24080
LeeBoonKong <lee.boon.kong@hotmail.com>
2017-12-06 16:22:35 +0800

    Home Screen update
    +Name, phone number and remaining credit will be shown on the home screen

--- app/src/main/java/unimyiw/carparkingmanagement2/HomeScreen.java
+++ app/src/main/java/unimyiw/carparkingmanagement2/HomeScreen.java
@@ -1,22 +1,61 @@
 package unimyiw.carparkingmanagement2;
 
 import android.content.Intent;
+import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class HomeScreen extends AppCompatActivity {
+
+    DatabaseReference mUserRef =  FirebaseDatabase.getInstance().getReference("users");
+    FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    TextView credit;
+    TextView phoneNo;
+    TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        userName = (TextView) findViewById(R.id.userName);
+        phoneNo = (TextView) findViewById(R.id.phoneNumber);
+        credit = (TextView) findViewById(R.id.credit);
+
+
+        mUserRef.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.child(currentFirebaseUser.getUid()).getValue(User.class);
+                        userName.setText(user.getName());
+                        phoneNo.setText(user.getPhoneNo());
+                        credit.setText(user.getCredit());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     @Override
